d356604d06f9bf4178985bcf257bf2100600d755
a-osiecki <a.osiecki@mi.unc.edu.ar>
2024-10-10 17:11:44 -0300

    implement deposit tx builder

--- src/offchain/handlers/deposit.ts
+++ src/offchain/handlers/deposit.ts
@@ -1,6 +1,13 @@
 import { DepositSchema } from "../../shared";
+import { DepositParams } from "../lib/params";
 import { deposit } from "../tx-builders/deposit";
-import { CBORHex, LucidEvolution, UTxO } from "@lucid-evolution/lucid";
+import {
+  CBORHex,
+  LucidEvolution,
+  selectUTxOs,
+  UTxO,
+} from "@lucid-evolution/lucid";
+import { env } from "../../config";
 
 async function handleDeposit(
   lucid: LucidEvolution,
@@ -13,11 +20,24 @@ async function handleDeposit(
   } = params;
   let fundsUtxo: UTxO | undefined = undefined;
   if (funds_utxo_ref) {
-    [fundsUtxo] = await lucid.utxosByOutRef([funds_utxo_ref]);
+    const { hash: txHash, index: outputIndex } = funds_utxo_ref;
+    [fundsUtxo] = await lucid.utxosByOutRef([{ txHash, outputIndex }]);
   }
-  const depositParams = { userAddress, amountToDeposit, fundsUtxo };
-  const unbTx = await deposit(lucid, depositParams);
-  return unbTx.toCBOR();
+  const walletUtxos = await lucid
+    .utxosAt(userAddress)
+    .then((utxos) => selectUTxOs(utxos, { lovelace: amountToDeposit }));
+  const [validatorRef] = await lucid.utxosByOutRef([
+    { txHash: env.VALIDATOR_REF, outputIndex: 0 },
+  ]);
+  const depositParams: DepositParams = {
+    userAddress,
+    amountToDeposit,
+    walletUtxos,
+    validatorRef,
+    fundsUtxo,
+  };
+  const unsignedTx = await deposit(lucid, depositParams);
+  return unsignedTx.toCBOR();
 }
 
 export { handleDeposit };

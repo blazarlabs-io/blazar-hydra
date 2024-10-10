f741af5860e1bd44e783a3d465a9e150ba19c801
a-osiecki <a.osiecki@mi.unc.edu.ar>
2024-10-10 18:02:45 -0300

    add error handling on handleDeposit function

--- src/offchain/handlers/deposit.ts
+++ src/offchain/handlers/deposit.ts
@@ -8,36 +8,51 @@ import {
   UTxO,
 } from "@lucid-evolution/lucid";
 import { env } from "../../config";
+import _ from "lodash";
+import { logger } from "../../logger";
 
 async function handleDeposit(
   lucid: LucidEvolution,
   params: DepositSchema
 ): Promise<CBORHex> {
-  const {
-    user_address: userAddress,
-    amount: amountToDeposit,
-    funds_utxo_ref,
-  } = params;
-  let fundsUtxo: UTxO | undefined = undefined;
-  if (funds_utxo_ref) {
-    const { hash: txHash, index: outputIndex } = funds_utxo_ref;
-    [fundsUtxo] = await lucid.utxosByOutRef([{ txHash, outputIndex }]);
+  try {
+    const {
+      user_address: userAddress,
+      amount: amountToDeposit,
+      funds_utxo_ref: fundsUtxoRef,
+    } = params;
+    const localLucid = _.cloneDeep(lucid);
+    let fundsUtxo: UTxO | undefined = undefined;
+    if (fundsUtxoRef) {
+      const { hash: txHash, index: outputIndex } = fundsUtxoRef;
+      [fundsUtxo] = await localLucid.utxosByOutRef([{ txHash, outputIndex }]);
+    }
+    const walletUtxos = await localLucid
+      .utxosAt(userAddress)
+      .then((utxos) => selectUTxOs(utxos, { lovelace: amountToDeposit }));
+    const [validatorRef] = await localLucid.utxosByOutRef([
+      { txHash: env.VALIDATOR_REF, outputIndex: 0 },
+    ]);
+    const depositParams: DepositParams = {
+      userAddress,
+      amountToDeposit,
+      walletUtxos,
+      validatorRef,
+      fundsUtxo,
+    };
+    const unsignedTx = await deposit(localLucid, depositParams);
+    return unsignedTx.toCBOR();
+  } catch (e) {
+    if (e instanceof Error) {
+      logger.error("500 /deposit - " + e.message);
+    } else if (typeof e === "string" && e.includes("InputsExhaustedError")) {
+      logger.error("400 /deposit - " + e);
+    } else {
+      logger.error("520 /deposit - Unknown error type");
+      logger.error(JSON.stringify(e));
+    }
+    throw e;
   }
-  const walletUtxos = await lucid
-    .utxosAt(userAddress)
-    .then((utxos) => selectUTxOs(utxos, { lovelace: amountToDeposit }));
-  const [validatorRef] = await lucid.utxosByOutRef([
-    { txHash: env.VALIDATOR_REF, outputIndex: 0 },
-  ]);
-  const depositParams: DepositParams = {
-    userAddress,
-    amountToDeposit,
-    walletUtxos,
-    validatorRef,
-    fundsUtxo,
-  };
-  const unsignedTx = await deposit(lucid, depositParams);
-  return unsignedTx.toCBOR();
 }
 
 export { handleDeposit };

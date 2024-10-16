ad04150a653168a35409e4c293925ac55f1c31d7
a-osiecki <a.osiecki@mi.unc.edu.ar>
2024-10-16 18:23:05 -0300

    implement withdraw handler

--- src/offchain/handlers/withdraw.ts
+++ src/offchain/handlers/withdraw.ts
@@ -1,38 +1,74 @@
 import { Layer, WithdrawSchema } from "../../shared";
 import { withdraw } from "../tx-builders/withdraw";
-import { CBORHex, LucidEvolution, UTxO } from "@lucid-evolution/lucid";
+import { LucidEvolution, selectUTxOs } from "@lucid-evolution/lucid";
 import { WithdrawParams } from "../lib/params";
+import { TxBuiltResponse } from "../../api/schemas/response";
+import _ from "lodash";
+import { env } from "../../config";
+import { logger } from "../../logger";
 
 async function handleWithdraw(
   lucid: LucidEvolution,
   params: WithdrawSchema
-): Promise<CBORHex> {
-  const {
-    address,
-    amount: amountToWithdraw,
-    funds_utxo_ref,
-    signature,
-    network_layer,
-  } = params;
-  if (network_layer === Layer.L1) {
-    throw new Error("Unsupported network layer for operation");
+): Promise<TxBuiltResponse> {
+  try {
+    const localLucid = _.cloneDeep(lucid);
+    const {
+      address,
+      owner,
+      funds_utxo_ref: fundsUtxoRef,
+      signature,
+      network_layer,
+    } = params;
+
+    // Lookup funds and validator UTxOs
+    const { hash: txHash, index: outputIndex } = fundsUtxoRef;
+    const [fundsUtxo] = await localLucid.utxosByOutRef([
+      { txHash, outputIndex },
+    ]);
+    if (!fundsUtxo) {
+      throw new Error(`Funds utxo not found in ${network_layer}`);
+    }
+    const [validatorRef] = await localLucid.utxosByOutRef([
+      { txHash: env.VALIDATOR_REF, outputIndex: 0 },
+    ]);
+
+    // Prepare tx builder parameters
+    let withdrawParams: WithdrawParams = {
+      address,
+      fundsUtxo,
+      signature,
+      validatorRef,
+    };
+    switch ([owner, network_layer]) {
+      case ["merchant", Layer.L2]:
+        break;
+
+      case ["user", Layer.L1]:
+        const walletUtxos = await localLucid
+          .utxosAt(address)
+          .then((utxos) => selectUTxOs(utxos, { lovelace: 10_000_000n }));
+        withdrawParams = { ...withdrawParams, walletUtxos };
+        break;
+
+      default:
+        throw new Error("Unsupported owner and network layer combination");
+    }
+
+    // Build and return the transaction
+    const { tx, newFundsUtxo } = await withdraw(localLucid, withdrawParams);
+    return { cborHex: tx.toCBOR(), fundsUtxoRef: newFundsUtxo };
+  } catch (e) {
+    if (e instanceof Error) {
+      logger.error("500 /withdraw - " + e.message);
+    } else if (typeof e === "string" && e.includes("InputsExhaustedError")) {
+      logger.error("400 /withdraw - " + e);
+    } else {
+      logger.error("520 /withdraw - Unknown error type");
+      logger.error(JSON.stringify(e));
+    }
+    throw e;
   }
-  let fundsUtxo: UTxO | undefined = undefined;
-  if (funds_utxo_ref) {
-    const { hash: txHash, index: outputIndex } = funds_utxo_ref;
-    [fundsUtxo] = await lucid.utxosByOutRef([{ txHash, outputIndex }]);
-  }
-  if (!fundsUtxo) {
-    throw new Error(`Funds utxo not found in ${network_layer.toString()}`);
-  }
-  const withdrawParams: WithdrawParams = {
-    address,
-    amountToWithdraw,
-    fundsUtxo,
-    signature,
-  };
-  const unbTx = await withdraw(lucid, withdrawParams);
-  return unbTx.toCBOR();
 }
 
 export { handleWithdraw };

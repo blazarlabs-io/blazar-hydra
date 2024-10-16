df9376716a255bff473fb122963890e9f950d688
a-osiecki <a.osiecki@mi.unc.edu.ar>
2024-10-16 18:23:19 -0300

    implement withdraw route

--- src/api/entry-points/routes.ts
+++ src/api/entry-points/routes.ts
@@ -1,8 +1,8 @@
 import e from "express";
 import { API_ROUTES } from "../schemas/routes";
 import { ERRORS } from "../schemas/errors";
-import { DepositZodSchema } from "../schemas/zod-schemas";
-import { handleDeposit } from "../../offchain";
+import { DepositZodSchema, WithdrawZodSchema } from "../schemas/zod";
+import { handleDeposit, handleWithdraw } from "../../offchain";
 import { LucidEvolution } from "@lucid-evolution/lucid";
 
 const setRoutes = (lucid: LucidEvolution, expressApp: e.Application) => {
@@ -23,8 +23,20 @@ const setRoutes = (lucid: LucidEvolution, expressApp: e.Application) => {
     }
   });
 
-  expressApp.post(API_ROUTES.WITHDRAW, (req, res) => {
-    res.send("Withdraw funds route");
+  expressApp.post(API_ROUTES.WITHDRAW, async (req, res) => {
+    try {
+      const withdrawSchema = WithdrawZodSchema.parse(req.body);
+      const _res = await handleWithdraw(lucid, withdrawSchema);
+      res.status(200).json(_res);
+    } catch (e) {
+      if (e instanceof Error) {
+        res.status(500).json({ error: `${ERRORS.INTERNAL_SERVER_ERROR}: ${e}` });
+      } else if (typeof e === "string" && e.includes("InputsExhaustedError")) {
+        res.status(400).json({ error: `${ERRORS.BAD_REQUEST}: ${e}` });
+      } else {
+        res.status(520).json({ error: `${ERRORS.INTERNAL_SERVER_ERROR}: ${e}` });
+      }
+    }
   });
 
   expressApp.post(API_ROUTES.PAY, (req, res) => {

f86071e55650d2b552c7670f4871ef57d827aa34
a-osiecki <a.osiecki@mi.unc.edu.ar>
2024-10-21 13:49:54 -0300

    WIP: commit funds tx builder

--- src/offchain/tx-builders/commit-funds.ts
+++ src/offchain/tx-builders/commit-funds.ts
@@ -1,12 +1,138 @@
-import { LucidEvolution, TxSignBuilder } from "@lucid-evolution/lucid";
+import {
+  CML,
+  createCostModels,
+  LucidEvolution,
+  TxSignBuilder,
+  utxoToCore,
+  validatorToRewardAddress,
+} from "@lucid-evolution/lucid";
 import { CommitFundsParams } from "../lib/params";
+import { Combined, Spend } from "../lib/types";
 
 async function commitFunds(
   lucid: LucidEvolution,
   params: CommitFundsParams
-): Promise<TxSignBuilder> {
-  const tx = await lucid.newTx().complete();
-  return tx;
+): Promise<{ tx: TxSignBuilder }> {
+  const { adminAddress, userFundUtxos, validatorRefUtxo } = params;
+
+  const validator = validatorRefUtxo.scriptRef;
+  if (!validator) {
+    throw new Error(`Validator not found at UTxO: ${validatorRefUtxo}`);
+  }
+  const rewardAddress = validatorToRewardAddress(
+    lucid.config().network,
+    validator
+  );
+  const sortedInputs = userFundUtxos.sort((a, b) => {
+    const ref1 = { hash: a.txHash, index: a.outputIndex };
+    const ref2 = { hash: b.txHash, index: b.outputIndex };
+    const hashComparison = ref1.hash.localeCompare(ref2.hash);
+    return hashComparison !== 0 ? hashComparison : ref1.index - ref2.index;
+  });
+
+  // Build Initial txbody
+  const inputs = CML.TransactionInputList.new();
+  sortedInputs.map((utxo) => {
+    const cmlInput = utxoToCore(utxo).input();
+    inputs.add(cmlInput);
+  });
+  const outputs = CML.TransactionOutputList.new();
+  const fee = 0n;
+  const txBody = CML.TransactionBody.new(inputs, outputs, fee);
+
+  // Add required signers
+  const signer = CML.Ed25519KeyHash.from_bech32(adminAddress);
+  const signers = CML.Ed25519KeyHashList.new();
+  signers.add(signer);
+  txBody.set_required_signers(signers);
+
+  // Add reference inputs
+  const refInput = utxoToCore(validatorRefUtxo).input();
+  const refInputs = CML.TransactionInputList.new();
+  refInputs.add(refInput);
+  txBody.set_reference_inputs(refInputs);
+
+  // Add withdrawal
+  const rewAddress = CML.RewardAddress.from_address(
+    CML.Address.from_bech32(rewardAddress)
+  );
+  if (!rewAddress) {
+    throw new Error(`Could not build reward address from script`);
+  }
+  const withdrawMap = CML.MapRewardAccountToCoin.new();
+  withdrawMap.insert(rewAddress, 0n);
+  txBody.set_withdrawals(withdrawMap);
+
+  // Create witness set
+  const txWitnessSet = CML.TransactionWitnessSet.new();
+
+  // Add spend redeemers
+  const legacyRedeemers = CML.LegacyRedeemerList.new();
+  sortedInputs.map((_, idx) => {
+    const tag = CML.RedeemerTag.Spend;
+    const index = BigInt(idx);
+    const data = CML.PlutusData.from_cbor_hex(Spend.Commit);
+    const units = CML.ExUnits.new(0n, 0n);
+    legacyRedeemers.add(CML.LegacyRedeemer.new(tag, index, data, units));
+  });
+  sortedInputs.map((_, idx) => {
+    const tag = CML.RedeemerTag.Spend;
+    const index = BigInt(idx);
+    const data = CML.PlutusData.from_cbor_hex(Spend.Commit);
+    const units = CML.ExUnits.new(0n, 0n);
+    legacyRedeemers.add(CML.LegacyRedeemer.new(tag, index, data, units));
+  });
+  // Add withdraw redeemer
+  legacyRedeemers.add(
+    CML.LegacyRedeemer.new(
+      CML.RedeemerTag.Reward,
+      0n,
+      CML.PlutusData.from_cbor_hex(Combined.CombinedCommit),
+      CML.ExUnits.new(0n, 0n)
+    )
+  );
+  const redeemers = CML.Redeemers.new_arr_legacy_redeemer(legacyRedeemers);
+  txWitnessSet.set_redeemers(redeemers);
+
+  // Add script data hash
+  const scriptDataHash = await getScriptDataHash(lucid, txWitnessSet);
+  if (scriptDataHash) {
+    txBody.set_script_data_hash(scriptDataHash);
+  } else {
+    throw new Error("Could not build script data hash");
+  }
+
+  const cmlTx = CML.Transaction.new(txBody, txWitnessSet, true).to_cbor_hex();
+  const tx = lucid.fromTx(cmlTx);
+
+  return { tx };
 }
+
+async function getScriptDataHash(
+  lucid: LucidEvolution,
+  txWitnessSet: CML.TransactionWitnessSet,
+): Promise<CML.ScriptDataHash | undefined> {
+  let scriptDataHash: CML.ScriptDataHash | undefined;
+  if (txWitnessSet.plutus_v3_scripts()) {
+    const costModels = lucid.config().protocolParameters.costModels;
+    //const plutusV3CostModels = costModels.PlutusV3;
+    const cmlCostModels = createCostModels(costModels);
+    // if (plutusV3CostModel) {
+    //   Object.entries(plutusV3CostModel)
+    //     .sort(([op1], [op2]) => op1.localeCompare(op2, "en"))
+    //     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    //     .forEach(([_, cost], i) => createCostModels.set(i, C.Int.new_i32(cost)));
+    // } else {
+    //   throw Error("Missing Cost model of PlutusV2");
+    // }
+    scriptDataHash = CML.hash_script_data(
+      txWitnessSet.redeemers()!,
+      cmlCostModels,
+      txWitnessSet.plutus_datums(),
+    );
+  }
+  return scriptDataHash;
+}
+
 
 export { commitFunds };

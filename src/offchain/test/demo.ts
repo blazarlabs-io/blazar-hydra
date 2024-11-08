3e128f9246fcae04615dd299d808e09d36bbcafa
a-osiecki <a.osiecki@mi.unc.edu.ar>
2024-11-08 13:24:35 -0300

    update test file

--- src/offchain/test/demo.ts
+++ src/offchain/test/demo.ts
@@ -11,10 +11,12 @@ import { env } from "../../config";
 import { handleDeposit } from "../handlers/deposit";
 import { commitFunds } from "../tx-builders/commit-funds";
 import { getPrivateKey, waitForUtxosUpdate } from "../lib/utils";
-import { HydraHandler, lucidUtxoToHydraUtxo } from "../lib/hydra";
+import { HydraHandler } from "../lib/hydra";
 import { Layer, WithdrawSchema } from "../../shared";
 import { handleWithdraw } from "../handlers/withdraw";
 import blake2b from "blake2b";
+import { handleOpenHead } from "../handlers/open-head";
+import { logger } from "../../logger";
 
 const adminSeed = env.SEED;
 const privKey = getPrivateKey(adminSeed);
@@ -25,20 +27,37 @@ const lucid = (await Lucid(
 lucid.selectWallet.fromSeed(adminSeed);
 const aliceWsUrl = "ws://127.0.0.1:4001";
 
-const openHead = async () => {
-  const hydra = new HydraHandler(lucid, aliceWsUrl);
-  const initTag = await hydra.init();
-  if (initTag !== "HeadIsInitializing") {
-    console.log("Head already initialized");
-  }
 
-  const adminAddress = await lucid.wallet().address();
+const aliceApiUrl = "http://127.0.0.1:4001/commit";
+const bobApiUrl = "http://127.0.0.1:4002/commit";
+
+logger.configureLogger(
+  {
+    level: "debug", //env.LOGGER_LEVEL,
+    prettyPrint: true, //env.PRETTY_PRINT,
+  },
+  false
+);
+
+const openHead = async () => {
+  lucid.selectWallet.fromSeed(adminSeed);
+  await handleOpenHead(lucid, {
+    auth_token: "",
+    peer_api_urls: [aliceApiUrl, bobApiUrl],
+  });
+};
+
+const deposit = async (fromWallet: 1 | 2) => {
+  const thisSeed = fromWallet === 1 ? env.SEED : env.USER_SEED;
+  lucid.selectWallet.fromSeed(thisSeed);
+  const address = await lucid.wallet().address();
+  const privKey = getPrivateKey(thisSeed);
   const publicKey = toHex(privKey.to_public().to_raw_bytes());
   let funds: OutRef[] = [];
   for (let i = 0; i < 2; i++) {
     console.log(`Creating a funds utxo with 10 ADA`);
     const depTx = await handleDeposit(lucid, {
-      user_address: adminAddress,
+      user_address: address,
       public_key: publicKey,
       amount: 10_000_000n,
     });
@@ -46,78 +65,14 @@ const openHead = async () => {
       .fromTx(depTx.cborHex)
       .sign.withWallet()
       .complete();
+    console.log(signedTx.toCBOR());
     const txHash = await signedTx.submit();
     console.log(`Submitted deposit tx with hash: ${txHash}`);
     funds.push(depTx.fundsUtxoRef!);
-    await waitForUtxosUpdate(lucid, txHash);
+    const addr = await lucid.wallet().address();
+    await waitForUtxosUpdate(lucid, addr, txHash);
   }
-  const userFundUtxos = await lucid.utxosByOutRef(
-    funds.map((outref) => ({ txHash: outref.txHash, outputIndex: 0 }))
-  );
-
-  const [validatorRef] = await lucid.utxosByOutRef([
-    { txHash: env.VALIDATOR_REF, outputIndex: 0 },
-  ]);
-
-  const commitTxAlice = await commitFunds(lucid, {
-    adminAddress: adminAddress,
-    userFundUtxos: [userFundUtxos[0]],
-    validatorRefUtxo: validatorRef,
-  });
-
-  const commitTxBob = await commitFunds(lucid, {
-    adminAddress: adminAddress,
-    userFundUtxos: [userFundUtxos[1]],
-    validatorRefUtxo: validatorRef,
-  });
-
-  /// Connect to hydra node
-  const aliceApiUrl = "http://127.0.0.1:4001/commit";
-  const bobApiUrl = "http://127.0.0.1:4002/commit";
-
-  //  Send commits to hydra node
-  const utxos1: [string, any][] = [
-    [
-      userFundUtxos[0].txHash + "#" + userFundUtxos[0].outputIndex,
-      lucidUtxoToHydraUtxo(userFundUtxos[0]),
-    ],
-  ];
-  const aliceCommitTxId = await hydra.sendCommit({
-    apiUrl: aliceApiUrl,
-    blueprint: commitTxAlice.tx.toCBOR(),
-    utxos: utxos1,
-  });
-  console.log(`Alice commit transaction submitted! tx id: ${aliceCommitTxId}`);
-  let aliceCommitTag = "";
-  console.info("Waiting for Alice commit to be confirmed by the hydra node");
-  while (aliceCommitTag !== "Committed") {
-    aliceCommitTag = await hydra.listen("Committed");
-  }
-
-  const utxos2: [string, any][] = [
-    [
-      userFundUtxos[1].txHash + "#" + userFundUtxos[1].outputIndex,
-      lucidUtxoToHydraUtxo(userFundUtxos[1]),
-    ],
-  ];
-  const bobCommitTxId = await hydra.sendCommit({
-    apiUrl: bobApiUrl,
-    blueprint: commitTxBob.tx.toCBOR(),
-    utxos: utxos2,
-  });
-  console.log(`Bob commit transaction submitted! tx id: ${bobCommitTxId}`);
-  let bobCommitTag = "";
-  console.info("Waiting for Bob commit to be confirmed by the hydra node");
-  while (bobCommitTag !== "Committed") {
-    bobCommitTag = await hydra.listen("Committed");
-  }
-
-  let openHeadTag = "";
-  while (openHeadTag !== "HeadIsOpen") {
-    console.log("Head not opened yet");
-    openHeadTag = await hydra.listen("HeadIsOpen");
-  }
-  await hydra.stop();
+  lucid.selectWallet.fromSeed(adminSeed);
 };
 
 const getSnapshot = async () => {
@@ -129,7 +84,7 @@ const getSnapshot = async () => {
 const closeHead = async () => {
   async function repeatCloseUntilSuccess(
     hydra: HydraHandler,
-    intervalMs: number = 30000
+    intervalMs: number = 10000
   ): Promise<string> {
     return new Promise((resolve, _) => {
       const attemptClose = async () => {
@@ -167,16 +122,16 @@ const fanout = async () => {
 const withdraw = async (fanoutTxId: string) => {
   const adminAddress = await lucid.wallet().address();
   const msg = Buffer.from(fanoutTxId + Data.to<bigint>(0n), "hex");
-  const hashedMsg =  blake2b(32).update(msg).digest("hex");
+  const hashedMsg = blake2b(32).update(msg).digest("hex");
   const sig = privKey.sign(Buffer.from(hashedMsg, "hex")).to_hex();
 
   const wSchema: WithdrawSchema = {
     address: adminAddress,
     owner: "user",
-    funds_utxos_ref: [{hash: fanoutTxId, index: 0}],
+    funds_utxos_ref: [{ hash: fanoutTxId, index: 0 }],
     signature: sig,
-    network_layer: Layer.L1
-  }
+    network_layer: Layer.L1,
+  };
   const withdrawTx = await handleWithdraw(lucid, wSchema);
   const signedTx = await lucid
     .fromTx(withdrawTx.cborHex)
@@ -184,7 +139,7 @@ const withdraw = async (fanoutTxId: string) => {
     .complete();
   const txHash = await signedTx.submit();
   console.log(`Submitted withdraw tx with hash: ${txHash}`);
-}
+};
 
 const abortHead = async () => {
   const hydra = new HydraHandler(lucid, aliceWsUrl);
@@ -195,6 +150,23 @@ const abortHead = async () => {
 
 const trace = process.env.npm_config_trace;
 switch (trace) {
+  case "deposit":
+    const wallet = process.env.npm_config_wallet;
+    if (!wallet) {
+      throw new Error("Missing wallet. Provide one with --wallet");
+    }
+    switch (wallet) {
+      case "admin":
+        await deposit(1);
+        break;
+      case "user":
+        await deposit(2);
+        break;
+      default:
+        console.log("Invalid or missing wallet option");
+        break;
+    }
+    break;
   case "open":
     await openHead();
     break;

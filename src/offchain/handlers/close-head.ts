053c748657f74a4742a97431e8f4e8def3675e2e
a-osiecki <a.osiecki@mi.unc.edu.ar>
2024-11-11 19:39:53 -0300

    WIP: implement close head handler

--- src/offchain/handlers/close-head.ts
+++ src/offchain/handlers/close-head.ts
@@ -1,15 +1,87 @@
-import { LucidEvolution } from "@lucid-evolution/lucid";
+import { Data, LucidEvolution } from "@lucid-evolution/lucid";
 import { ManageHeadSchema } from "../../shared";
+import { logger } from "../../logger";
+import { HydraHandler } from "../lib/hydra";
+import _ from "lodash";
+import { env } from "../../config";
+import { FundsDatum, FundsDatumT } from "../lib/types";
+import { WithdrawParams } from "../lib/params";
+import { withdraw } from "../tx-builders/withdraw";
 
 async function handleCloseHead(
   lucid: LucidEvolution,
   params: ManageHeadSchema
 ): Promise<void> {
-  const { auth_token } = params;
-  if (!validateAdmin(auth_token)) {
-    throw new Error("Unauthorized");
+  try {
+    const { auth_token } = params;
+    const { ADMIN_KEY: adminKey, HYDRA_KEY: hydraKey } = env;
+    if (!validateAdmin(auth_token)) {
+      throw new Error("Unauthorized");
+    }
+    const { ADMIN_ADDRESS: adminAddress, ADMIN_NODE_WS_URL: wsUrl } = env;
+    const localLucid = _.cloneDeep(lucid);
+    localLucid.selectWallet.fromSeed(env.SEED);
+    const hydra = new HydraHandler(localLucid, wsUrl);
+    let currentExpectedTag = "";
+
+    // Step 1: Withdraw Merchant utxos
+    const fundUtxos = await hydra.getSnapshot();
+    const merchantUtxos = fundUtxos.filter((utxo) => {
+      const datum = Data.from<FundsDatumT>(utxo.datum!, FundsDatum);
+      return datum.funds_type === "Merchant";
+    });
+    if (merchantUtxos.length !== 0) {
+      logger.info("Withdrawing merchant utxos...");
+      const walletUtxos = await localLucid.utxosAt(adminAddress);
+      const withdrawParams: WithdrawParams = {
+        kind: "merchant",
+        fundsUtxos: merchantUtxos,
+        adminKey,
+        hydraKey,
+        walletUtxos,
+      };
+      const { tx } = await withdraw(localLucid, withdrawParams);
+      const signedTx = await tx.sign
+        .withWallet()
+        .complete()
+        .then((tx) => tx.toCBOR());
+      while (currentExpectedTag !== "TxValid") {
+        currentExpectedTag = await hydra.sendTx(signedTx);
+        if (currentExpectedTag !== "TxValid") {
+          throw new Error("Tx is not valid, retrying...");
+        }
+        _.delay(() => {}, 10000);
+      }
+    }
+
+    // Step 2: Send close command
+    logger.info("Closing head...");
+    while (currentExpectedTag !== "HeadIsClosed") {
+      currentExpectedTag = (await Promise.race([
+        new Promise((resolve, _) =>
+          setTimeout(() => {
+            logger.error("Close command not received, retrying...");
+            resolve("IncorrectTag");
+          }, 40_000)
+        ),
+        hydra.close(),
+      ])) as string;
+      hydra.stop();
+    }
+    logger.info("Waiting for fanout tag...");
+    while (currentExpectedTag !== "ReadyToFanout") {
+      currentExpectedTag = await hydra.listen("ReadyToFanout");
+    }
+
+    // Step 3: Fanout
+    logger.info("Sending fanout command...");
+    await hydra.fanout();
+    hydra.stop();
+    return;
+  } catch (error) {
+    logger.error("Error during close head");
+    throw error;
   }
-  return;
 }
 
 function validateAdmin(auth_token: string): boolean {
--- src/offchain/handlers/open-head.ts
+++ src/offchain/handlers/open-head.ts
@@ -31,134 +31,143 @@ async function handleOpenHead(
     ADMIN_NODE_WS_URL: wsUrl,
     VALIDATOR_REF: vRef,
   } = env;
-  if (!validateAdmin(auth_token)) {
-    throw new Error("Unauthorized");
-  }
-  const localLucid = _.cloneDeep(lucid);
-  localLucid.selectWallet.fromSeed(env.SEED);
-
-  // Step 1: Initialize the head
-  logger.info("Initializing head...");
-  const hydra = new HydraHandler(localLucid, wsUrl);
-  const initTag = await hydra.init();
-  if (initTag !== "HeadIsInitializing") {
-    logger.error("Head already initialized");
-  }
-
-  // Step 2: Lookup deposit UTxOs in L1 and merge them for each user
-  const [validatorRef] = await localLucid.utxosByOutRef([
-    { txHash: vRef, outputIndex: 0 },
-  ]);
-  const validator = getValidator(validatorRef);
-  const scriptAddress = validatorToAddress(
-    localLucid.config().network,
-    validator
-  );
-  const scriptUtxos = await localLucid.utxosAt(scriptAddress);
-  if (scriptUtxos.length === 0) {
-    throw new Error("No deposits to commit");
-  }
-  const userToDepositsMap = new Map<string, UTxO[]>();
-  for (const utxo of scriptUtxos) {
-    const { addr } = Data.from<FundsDatumT>(utxo.datum!, FundsDatum);
-    const userAddress = dataAddressToBech32(localLucid, addr);
-    if (!userToDepositsMap.has(userAddress)) {
-      userToDepositsMap.set(userAddress, []);
+  try {
+    if (!validateAdmin(auth_token)) {
+      throw new Error("Unauthorized");
     }
-    userToDepositsMap.get(userAddress)!.push(utxo);
-  }
-  let currentAdminUtxos = await localLucid.utxosAt(adminAddress).then((utxos) =>
-    selectUTxOs(utxos, {
-      ["lovelace"]: BigInt(userToDepositsMap.size * 1_000_000 + 5_000_000),
-    })
-  );
-  let mergeTxs: string[] = [];
-  let fundsRefs: OutRef[] = [];
-  logger.info("Preparing merge transactions...");
-  for (const [_, deposits] of userToDepositsMap) {
-    if (deposits.length === 1) {
-      // User has only one funds utxo, no need for a merge transaction
-      fundsRefs.push(deposits.pop()!);
-      continue;
+    const localLucid = _.cloneDeep(lucid);
+    localLucid.selectWallet.fromSeed(env.SEED);
+
+    // Step 1: Initialize the head
+    logger.info("Initializing head...");
+    const hydra = new HydraHandler(localLucid, wsUrl);
+    const initTag = await hydra.init();
+    if (initTag !== "HeadIsInitializing") {
+      logger.error("Head already initialized");
     }
-    const params = {
-      adminAddress,
-      userFundsUtxos: deposits,
-      adminUtxos: currentAdminUtxos,
-      validatorRef: validatorRef,
-    };
-    const { tx, newFundsUtxo, newAdminUtxos } = await mergeFunds(
-      localLucid,
-      params
+
+    // Step 2: Lookup deposit UTxOs in L1 and merge them for each user
+    const [validatorRef] = await localLucid.utxosByOutRef([
+      { txHash: vRef, outputIndex: 0 },
+    ]);
+    const validator = getValidator(validatorRef);
+    const scriptAddress = validatorToAddress(
+      localLucid.config().network,
+      validator
     );
-    const signedTx = await tx.sign
-      .withWallet()
-      .complete()
-      .then((tx) => tx.toCBOR());
-    fundsRefs.push(newFundsUtxo);
-    mergeTxs.push(signedTx);
-    currentAdminUtxos = newAdminUtxos;
-  }
-  if (mergeTxs.length > 0) {
-    logger.info("Submitting merge transactions...");
-    for (const tx of mergeTxs) {
-      const txid = await localLucid.wallet().submitTx(tx);
-      logger.info(
-        `Merge transaction submitted! tx id: https://preprod.cexplorer.io/tx/${txid}`
+    const scriptUtxos = await localLucid.utxosAt(scriptAddress);
+    if (scriptUtxos.length === 0) {
+      throw new Error("No deposits to commit");
+    }
+    const userToDepositsMap = new Map<string, UTxO[]>();
+    for (const utxo of scriptUtxos) {
+      const { addr } = Data.from<FundsDatumT>(utxo.datum!, FundsDatum);
+      const userAddress = dataAddressToBech32(localLucid, addr);
+      if (!userToDepositsMap.has(userAddress)) {
+        userToDepositsMap.set(userAddress, []);
+      }
+      userToDepositsMap.get(userAddress)!.push(utxo);
+    }
+    let currentAdminUtxos = await localLucid
+      .utxosAt(adminAddress)
+      .then((utxos) =>
+        selectUTxOs(utxos, {
+          ["lovelace"]: BigInt(userToDepositsMap.size * 1_000_000 + 5_000_000),
+        })
       );
+    let mergeTxs: string[] = [];
+    let fundsRefs: OutRef[] = [];
+    logger.info("Preparing merge transactions...");
+    for (const [_, deposits] of userToDepositsMap) {
+      if (deposits.length === 1) {
+        // User has only one funds utxo, no need for a merge transaction
+        fundsRefs.push(deposits.pop()!);
+        continue;
+      }
+      const params = {
+        adminAddress,
+        userFundsUtxos: deposits,
+        adminUtxos: currentAdminUtxos,
+        validatorRef: validatorRef,
+      };
+      const { tx, newFundsUtxo, newAdminUtxos } = await mergeFunds(
+        localLucid,
+        params
+      );
+      const signedTx = await tx.sign
+        .withWallet()
+        .complete()
+        .then((tx) => tx.toCBOR());
+      fundsRefs.push(newFundsUtxo);
+      mergeTxs.push(signedTx);
+      currentAdminUtxos = newAdminUtxos;
     }
-    const lastSubmittedTxHash = localLucid
-      .fromTx(mergeTxs[mergeTxs.length - 1])
-      .toHash();
-    logger.info(
-      "Merge transactions submitted succesfully, last tx: " +
-        lastSubmittedTxHash
-    );
-    await waitForUtxosUpdate(localLucid, adminAddress, lastSubmittedTxHash);
-  }
+    if (mergeTxs.length > 0) {
+      logger.info("Submitting merge transactions...");
+      for (const tx of mergeTxs) {
+        const txid = await localLucid.wallet().submitTx(tx);
+        logger.info(
+          `Merge transaction submitted! tx id: https://preprod.cexplorer.io/tx/${txid}`
+        );
+      }
+      const lastSubmittedTxHash = localLucid
+        .fromTx(mergeTxs[mergeTxs.length - 1])
+        .toHash();
+      logger.info(
+        "Merge transactions submitted succesfully, last tx: " +
+          lastSubmittedTxHash
+      );
+      await waitForUtxosUpdate(localLucid, adminAddress, lastSubmittedTxHash);
+    }
 
-  // Step 3: Commit the funds
-  const adminUtxos = await localLucid
-    .utxosAt(adminAddress)
-    .then((utxos) => utxos.filter((utxo) => utxo.assets["lovelace"] > 20_000_000));
-  const adminCollateral = adminUtxos[0];
-  const utxosToCommit = await localLucid.utxosByOutRef(fundsRefs);
-  const utxosPerPeer = utxosToCommit.length / peerUrls.length;
-  for (let i = 0; i < peerUrls.length; i++) {
-    const peerUrl = peerUrls[i];
-    const thisPeerUtxos = utxosToCommit.slice(0, utxosPerPeer);
-    utxosToCommit.splice(0, utxosPerPeer);
-    let params: CommitFundsParams = {
-      adminAddress,
-      userFundUtxos: thisPeerUtxos,
-      validatorRefUtxo: validatorRef,
-    };
-    // Add admin collateral to the last commit tx
-    if (i === peerUrls.length - 1) {
-      params = { ...params, adminCollateral };
+    // Step 3: Commit the funds
+    const adminUtxos = await localLucid
+      .utxosAt(adminAddress)
+      .then((utxos) =>
+        utxos.filter((utxo) => utxo.assets["lovelace"] > 20_000_000)
+      );
+    const adminCollateral = adminUtxos[0];
+    const utxosToCommit = await localLucid.utxosByOutRef(fundsRefs);
+    const utxosPerPeer = utxosToCommit.length / peerUrls.length;
+    for (let i = 0; i < peerUrls.length; i++) {
+      const peerUrl = peerUrls[i];
+      const thisPeerUtxos = utxosToCommit.slice(0, utxosPerPeer);
+      utxosToCommit.splice(0, utxosPerPeer);
+      let params: CommitFundsParams = {
+        adminAddress,
+        userFundUtxos: thisPeerUtxos,
+        validatorRefUtxo: validatorRef,
+      };
+      // Add admin collateral to the last commit tx
+      if (i === peerUrls.length - 1) {
+        params = { ...params, adminCollateral };
+      }
+      const { tx } = await commitFunds(localLucid, params);
+      const peerCommitTxId = await hydra.sendCommit(
+        peerUrl,
+        tx.toCBOR(),
+        thisPeerUtxos
+      );
+      logger.info(`Commit transaction submitted! tx id: ${peerCommitTxId}`);
+      let commitTag = "";
+      logger.info("Waiting for last commit to be confirmed by the hydra node");
+      while (commitTag !== "Committed") {
+        commitTag = await hydra.listen("Committed");
+      }
     }
-    const { tx } = await commitFunds(localLucid, params);
-    const peerCommitTxId = await hydra.sendCommit(
-      peerUrl,
-      tx.toCBOR(),
-      thisPeerUtxos
-    );
-    logger.info(`Commit transaction submitted! tx id: ${peerCommitTxId}`);
-    let commitTag = "";
-    logger.info("Waiting for last commit to be confirmed by the hydra node");
-    while (commitTag !== "Committed") {
-      commitTag = await hydra.listen("Committed");
-    }
-  }
-  logger.info("All funds committed successfully");
+    logger.info("All funds committed successfully");
 
-  let openHeadTag = "";
-  while (openHeadTag !== "HeadIsOpen") {
-    logger.info("Head not opened yet");
-    openHeadTag = await hydra.listen("HeadIsOpen");
+    let openHeadTag = "";
+    while (openHeadTag !== "HeadIsOpen") {
+      logger.info("Head not opened yet");
+      openHeadTag = await hydra.listen("HeadIsOpen");
+    }
+    await hydra.stop();
+    return;
+  } catch (error) {
+    logger.error("Error during open head");
+    throw error;
   }
-  await hydra.stop();
-  return;
 }
 
 function validateAdmin(auth_token: string): boolean {

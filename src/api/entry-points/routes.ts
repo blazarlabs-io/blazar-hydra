fc30130532e1475f8623a2eb11289d5d219ae582
a-osiecki <a.osiecki@mi.unc.edu.ar>
2024-11-20 14:46:21 -0300

    implement routes for open and close head

--- src/api/entry-points/routes.ts
+++ src/api/entry-points/routes.ts
@@ -3,11 +3,14 @@ import { API_ROUTES } from "../schemas/routes";
 import { ERRORS } from "../schemas/errors";
 import {
   DepositZodSchema,
+  ManageHeadZodSchema,
   PayMerchantZodSchema,
   WithdrawZodSchema,
 } from "../schemas/zod";
 import {
+  handleCloseHead,
   handleDeposit,
+  handleOpenHead,
   handlePay,
   handleQueryFunds,
   handleWithdraw,
@@ -88,12 +91,44 @@ const setRoutes = (lucid: LucidEvolution, expressApp: e.Application) => {
   });
 
   // Admin Routes
-  expressApp.post(API_ROUTES.OPEN_HEAD, (req, res) => {
-    res.send("Open hydra head route");
+  expressApp.post(API_ROUTES.OPEN_HEAD, async (req, res) => {
+    try {
+      const openHeadSchema = ManageHeadZodSchema.parse(req.body);
+      const _res = await handleOpenHead(lucid, openHeadSchema);
+      res.status(200).json(_res);
+    } catch (e) {
+      if (e instanceof Error) {
+        res
+          .status(500)
+          .json({ error: `${ERRORS.INTERNAL_SERVER_ERROR}: ${e}` });
+      } else if (typeof e === "string" && e.includes("InputsExhaustedError")) {
+        res.status(400).json({ error: `${ERRORS.BAD_REQUEST}: ${e}` });
+      } else {
+        res
+          .status(520)
+          .json({ error: `${ERRORS.INTERNAL_SERVER_ERROR}: ${e}` });
+      }
+    }
   });
 
-  expressApp.post(API_ROUTES.CLOSE_HEAD, (req, res) => {
-    res.send("Close hydra head route");
+  expressApp.post(API_ROUTES.CLOSE_HEAD, async (req, res) => {
+    try {
+      const closeHeadSchema = ManageHeadZodSchema.parse(req.body);
+      const _res = await handleCloseHead(lucid, closeHeadSchema);
+      res.status(200).json(_res);
+    } catch (e) {
+      if (e instanceof Error) {
+        res
+          .status(500)
+          .json({ error: `${ERRORS.INTERNAL_SERVER_ERROR}: ${e}` });
+      } else if (typeof e === "string" && e.includes("InputsExhaustedError")) {
+        res.status(400).json({ error: `${ERRORS.BAD_REQUEST}: ${e}` });
+      } else {
+        res
+          .status(520)
+          .json({ error: `${ERRORS.INTERNAL_SERVER_ERROR}: ${e}` });
+      }
+    }
   });
 };
 

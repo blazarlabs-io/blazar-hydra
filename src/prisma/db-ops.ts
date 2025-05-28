62d0425eb4e579a901045a72e5fe92eec51dbc96
a-osiecki <a.osiecki@mi.unc.edu.ar>
2025-05-28 12:57:00 -0300

    modularize db ops

---
+++ src/prisma/db-ops.ts
@@ -0,0 +1,32 @@
+import { prisma } from "../config";
+import { logger } from "../logger";
+import { DBStatus } from "../shared/prisma-schemas";
+
+export namespace DBOps {
+  export const newHead = async () => {
+    const newProcess = await prisma.process
+      .create({
+        data: {
+          status: DBStatus.INITIALIZING,
+        },
+      })
+      .catch((error) => {
+        logger.error("DB Error while opening head: " + error);
+        throw error;
+      });
+    return newProcess.id;
+  };
+
+  export const updateHeadStatus = async (id: string, status: DBStatus) => {
+    await prisma.process
+      .upsert({
+        where: { id },
+        update: { status },
+        create: { id, status },
+      })
+      .catch((error) => {
+        logger.error(`DB Error while updating status to ${status}: ${error}`);
+        throw error;
+      });
+  };
+}

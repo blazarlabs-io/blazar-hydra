f5c24c9a8dee8e5809bc35cf92ce9cb8f2001c86
a-osiecki <a.osiecki@mi.unc.edu.ar>
2025-06-04 13:20:13 -0300

    update demo script

--- src/offchain/test/demo.ts
+++ src/offchain/test/demo.ts
@@ -7,21 +7,20 @@ import {
   Network,
   OutRef,
   toHex,
+  validatorToAddress,
 } from "@lucid-evolution/lucid";
 import { env } from "../../config";
 import { handleDeposit } from "../handlers/deposit";
-import { commitFunds } from "../tx-builders/commit-funds";
 import {
   bech32ToAddressType,
   dataAddressToBech32,
+  getNetworkFromLucid,
   getPrivateKey,
   waitForUtxosUpdate,
 } from "../lib/utils";
 import { HydraHandler } from "../lib/hydra";
 import { Layer, PayMerchantSchema, WithdrawSchema } from "../../shared";
 import { handleWithdraw } from "../handlers/withdraw";
-import blake2b from "blake2b";
-import { handleOpenHead } from "../handlers/open-head";
 import { logger } from "../../logger";
 import {
   FundsDatum,
@@ -31,17 +30,14 @@ import {
   WithdrawInfo,
   WithdrawInfoT,
 } from "../lib/types";
-import { handlePay } from "../handlers/pay-merchant";
-import { handleCloseHead } from "../handlers/close-head";
 import axios from "axios";
 import JSONbig from "json-bigint";
 import { API_ROUTES } from "../../api/schemas/routes";
 
 const adminSeed = env.SEED;
-const privKey = getPrivateKey(adminSeed);
 const lucid = (await Lucid(
   new Blockfrost(env.PROVIDER_URL, env.PROVIDER_PROJECT_ID),
-  env.NETWORK as Network,
+  env.NETWORK as Network
 )) as LucidEvolution;
 lucid.selectWallet.fromSeed(adminSeed);
 const aliceWsUrl = "ws://127.0.0.1:4001";
@@ -83,13 +79,12 @@ logger.configureLogger(
     level: "debug", //env.LOGGER_LEVEL,
     prettyPrint: true, //env.PRETTY_PRINT,
   },
-  false,
+  false
 );
 
 const openHead = async () => {
   lucid.selectWallet.fromSeed(adminSeed);
   const { operationId } = await postEp(ownServerUrl + API_ROUTES.OPEN_HEAD, {
-    auth_token: "",
     peer_api_urls: [aliceApiUrl, bobApiUrl],
   });
   logger.debug(`Operation ID: ${operationId}`);
@@ -133,7 +128,7 @@ const pay = async (
   amount: bigint,
   from: Address,
   to: Address,
-  withWallet: 1 | 2,
+  withWallet: 1 | 2
 ) => {
   const hydra = new HydraHandler(lucid, aliceWsUrl);
   const utxos = await hydra.getSnapshot();
@@ -179,35 +174,65 @@ const fanout = async () => {
   await hydra.stop();
 };
 
-const withdraw = async (fanoutTxId: string) => {
-  const adminAddress = await lucid.wallet().address();
-  const withdrawInfo: WithdrawInfoT = {
-    ref: {
-      transaction_id: fanoutTxId,
-      output_index: 0n,
-    },
-  };
-  const msg = Buffer.from(
-    Data.to<WithdrawInfoT>(withdrawInfo, WithdrawInfo),
-    "hex",
-  );
-  const hashedMsg = blake2b(32).update(msg).digest("hex");
-  const sig = privKey.sign(Buffer.from(hashedMsg, "hex")).to_hex();
+const withdraw = async (address: Address, seed: string) => {
+  const [validatorUtxo] = await lucid.utxosByOutRef([
+    { txHash: env.VALIDATOR_REF, outputIndex: 0 },
+  ]);
+  const validator = validatorUtxo.scriptRef!;
+  const network = getNetworkFromLucid(lucid);
+  const scriptAddress = validatorToAddress(network, validator);
+  const withdrawInfos: WithdrawInfoT[] = await lucid
+    .utxosAt(scriptAddress)
+    .then((utxos) =>
+      utxos.filter((utxo) => {
+        const dat = utxo.datum;
+        if (!dat) {
+          return false;
+        }
+        const datum = Data.from<FundsDatumT>(dat, FundsDatum);
+        if (datum.funds_type === "Merchant") {
+          return false;
+        }
+        return dataAddressToBech32(lucid, datum.addr) === address;
+      })
+    )
+    .then((utxos) =>
+      utxos.map((u) => {
+        return {
+          ref: {
+            transaction_id: u.txHash,
+            output_index: BigInt(u.outputIndex),
+          },
+        };
+      })
+    );
+
+  const withdraws = withdrawInfos.map((w) => {
+    const msg = Buffer.from(Data.to<WithdrawInfoT>(w, WithdrawInfo), "hex");
+    const sig = getPrivateKey(seed).sign(msg).to_hex();
+    return {
+      ref: { hash: w.ref.transaction_id, index: Number(w.ref.output_index) },
+      signature: sig,
+    };
+  });
 
   const wSchema: WithdrawSchema = {
-    address: adminAddress,
+    address: address,
     owner: "user",
-    funds_utxos_ref: [{ hash: fanoutTxId, index: 0 }],
-    signature: sig,
+    funds_utxos: withdraws,
     network_layer: Layer.L1,
   };
+  lucid.selectWallet.fromSeed(seed);
   const withdrawTx = await handleWithdraw(lucid, wSchema);
+  console.log(withdrawTx);
+  // Sign user
   const signedTx = await lucid
     .fromTx(withdrawTx.cborHex)
     .sign.withWallet()
     .complete();
   const txHash = await signedTx.submit();
   console.log(`Submitted withdraw tx with hash: ${txHash}`);
+  lucid.selectWallet.fromSeed(adminSeed);
 };
 
 const abortHead = async () => {
@@ -251,9 +276,8 @@ switch (trace) {
     const closeRes = await postEp(
       `${ownServerUrl}${API_ROUTES.CLOSE_HEAD}/?id=${id}`,
       {
-        auth_token: "",
         peer_api_urls: [aliceApiUrl, bobApiUrl],
-      },
+      }
     );
     break;
   case "pay":
@@ -265,12 +289,12 @@ switch (trace) {
     }
     if (!mAddr) {
       throw new Error(
-        "Missing merchant address. Provide with --merchant-address",
+        "Missing merchant address. Provide with --merchant-address"
       );
     }
     if (!user) {
       throw new Error(
-        "User not specified. Provide with --from. Options: user1, user2",
+        "User not specified. Provide with --from. Options: user1, user2"
       );
     }
     const withWallet = user === "user1" ? 1 : 2;
@@ -281,30 +305,27 @@ switch (trace) {
     await fanout();
     break;
   case "withdraw":
-    const txId = process.env.npm_config_fanout_txid;
-    if (!txId) {
-      throw new Error("Missing txid. Provide one with --fanout_txid");
+    const from = process.env.npm_config_from;
+    if (!from) {
+      throw new Error("Missing from. Provide one with --from");
     }
-    await withdraw(txId);
+    const wallet = from === "user1" ? 1 : 2;
+    const addr = wallet === 1 ? env.USER_ADDRESS : env.USER_ADDRESS_2;
+    const seed = wallet === 1 ? env.USER_SEED : env.USER_SEED_2;
+    await withdraw(addr, seed);
     break;
   case "paymany":
     const hydra = new HydraHandler(lucid, aliceWsUrl);
     const utxos = await hydra.getSnapshot();
-    const magia = utxos
+    utxos
       .filter((utxo) => {
         const dat = utxo.datum;
-
         if (!dat) {
           return false;
         }
-
-        if (
-          utxo.address ==
-          "addr_test1qztpj076fax0h3hy7vekhzuls2ezd7kh3mphanxh944yuhka76a8nz64ccpusr9q0w7q7kt2ze49d4dtu8564a2m23as8k20j9"
-        ) {
+        if (utxo.address == env.ADMIN_ADDRESS) {
           return false;
         }
-
         const type = Data.from<FundsDatumT>(dat, FundsDatum).funds_type;
         return type != "Merchant";
       })
@@ -312,14 +333,10 @@ switch (trace) {
         return utxo.txHash + "#" + utxo.outputIndex;
       })
       .forEach(async (ref) => {
-        await pay(
-          2000000n,
-          "addr_test1qpkxq49y8vv5vwmacfs58h9dr6tzmdet8e4jvp5dkxxmaaqx69fzeuykylvmlcaav5eyp49stczujq0c2xxv83eukf5sc0ed6m",
-          ref,
-          2,
-        );
+        await pay(2000000n, env.USER_ADDRESS_2, env.USER_ADDRESS, 2);
+        await hydra.listen("TxValid")
       });
-    console.dir(magia, { depth: null });
+    console.dir("Many payments done", { depth: null });
     await hydra.stop();
     break;
   default:

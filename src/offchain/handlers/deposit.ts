10dc3ded7fbaa2c26a8be0c2c008f3018c00f6df
a-osiecki <a.osiecki@mi.unc.edu.ar>
2025-07-23 16:10:51 -0300

    submit deposit txs

--- src/offchain/handlers/deposit.ts
+++ src/offchain/handlers/deposit.ts
@@ -50,6 +50,10 @@ async function handleDeposit(
       depositParams,
       adminAddress
     );
+
+    logger.info(`Submitting deposit transaction with id ${tx.toHash()}`);
+    (await tx.sign.withWallet().complete()).submit();
+    logger.info(`Deposit transaction ${tx.toHash()} submitted successfully`);
     return { cborHex: tx.toCBOR(), fundsUtxoRef: newFundsUtxo };
   } catch (e) {
     if (e instanceof Error) {

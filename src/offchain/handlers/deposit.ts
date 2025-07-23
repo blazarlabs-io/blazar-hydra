1b3a33964e1ec7f8170929b3f2024d26bb335603
a-osiecki <a.osiecki@mi.unc.edu.ar>
2025-07-23 16:12:00 -0300

    set wallet before signing

--- src/offchain/handlers/deposit.ts
+++ src/offchain/handlers/deposit.ts
@@ -30,40 +30,6 @@ async function handleDeposit(
     [fundsUtxo] = await localLucid.utxosByOutRef([
       { txHash, outputIndex: Number(index) },
     ]);
-<<<<<<< HEAD
-=======
-    const nonEmptyPubKey =
-      publicKey && publicKey.length > 0 ? publicKey : '0'.repeat(64);
-    const depositParams: DepositParams = {
-      userAddress,
-      publicKey: nonEmptyPubKey,
-      amountToDeposit,
-      walletUtxos,
-      validatorRef,
-      fundsUtxo,
-    };
-    const { tx, newFundsUtxo } = await deposit(
-      localLucid,
-      depositParams,
-      adminAddress
-    );
-
-    logger.info(`Submitting deposit transaction with id ${tx.toHash()}`);
-    lucid.selectWallet.fromSeed(env.SEED);
-    (await tx.sign.withWallet().complete()).submit();
-    logger.info(`Deposit transaction ${tx.toHash()} submitted successfully`);
-    return { cborHex: tx.toCBOR(), fundsUtxoRef: newFundsUtxo };
-  } catch (e) {
-    if (e instanceof Error) {
-      logger.error('500 /deposit - ' + e.message);
-    } else if (typeof e === 'string' && e.includes('InputsExhaustedError')) {
-      logger.error('400 /deposit - ' + e);
-    } else {
-      logger.error('520 /deposit - Unknown error type');
-      logger.error(JSON.stringify(e));
-    }
-    throw e;
->>>>>>> 10dc3de (submit deposit txs)
   }
 
   lucid.selectWallet.fromSeed(env.SEED);
@@ -101,6 +67,7 @@ async function handleDeposit(
   );
 
   logger.info(`Submitting deposit transaction with id ${tx.toHash()}`);
+  lucid.selectWallet.fromSeed(env.SEED);
   (await tx.sign.withWallet().complete()).submit();
   logger.info(`Deposit transaction ${tx.toHash()} submitted successfully`);
   return { cborHex: tx.toCBOR(), fundsUtxoRef: newFundsUtxo };
